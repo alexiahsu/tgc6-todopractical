00c66752706f8b52025c8651b7be41c626e13ca7
alexiahsu <hsu.enyang@gmail.com>
2020-07-04 04:06:30 +0000

    full to-do process and added js

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Requirements
+# Requirements needed
 
 ```
 click==7.1.2
@@ -9,3 +9,29 @@ pymongo==3.10.1
 python-dotenv==0.13.0
 Werkzeug==1.0.1
 ```
+
+* `pip3 install flask`
+* `pip3 install pymongo` -- to use Mongo DB
+* `pip3 install dnspython` -- allows us to connect to Mongo with just the URL
+* `pip3 install python-dotenv` -- allows the use of `.env` files for environment variables
+
+## How to use requirements.txt
+```
+pip3 install -r requirements.txt
+```
+
+# Todos
+What fields or information do we want to track for each Todos
+* Task name
+* Due date
+* Whether it is done
+* Comment
+
+# SESSION KEYS
+Generated from https://randomkeygen.com/
+
+# Flash messages
+1. See documentation (different class for errors) https://flask.palletsprojects.com/en/1.1.x/patterns/flashing/
+2. Ensure sessions are enabled -> we have to make sure 
+`app.secret_key` has been set
+3. In the `layout.template.html` add in the code to display the flash messages
--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
 import pymongo
 from dotenv import load_dotenv
+from bson import ObjectId
 import os
 import datetime
 
@@ -16,20 +17,35 @@ client = pymongo.MongoClient(MONGO_URI)
 # Define db name
 DB_NAME = "todolist"
 
+# Read in the session_key variable from the operating system environment
+SESSION_KEY = os.environ.get('SESSION_KEY')
+
+# Set the session_key
+app.secret_key = SESSION_KEY
+
+# Home route
+# Display all the tasks
+
+
 @app.route('/')
 def home():
-    return "Welcome home"
+    tasks = client[DB_NAME].todos.find()
+    return render_template('home.template.html', tasks=tasks)
 
 # FOR "C" in CRUD
-## One route to show the form and ask the user to type in
-## One route to process the form (extract the data) & send to database
+# One route to show the form and ask the user to type in
+# One route to process the form (extract the data) & send to database
 
-### Show form
+# Show form
+
+
 @app.route('/tasks/create')
 def show_create_form():
     return render_template('create_task.template.html')
 
-### Process form (extract data) and write into mongo db
+# Process form (extract data) and write into mongo db
+
+
 @app.route('/tasks/create', methods=["POST"])
 def create_task():
     task_name = request.form.get('task-name')
@@ -39,14 +55,38 @@ def create_task():
     client[DB_NAME].todos.insert_one({
         'task_name': task_name,
         'due_date': datetime.datetime.strptime(due_date, "%Y-%m-%d"),
-        'comments': comments
+        'comments': comments,
+        'done': False
+    })
+    flash(f"New task '{task_name}' has been created")
+    return redirect(url_for('home'))
+
+
+@app.route('/tasks/check', methods=["PATCH"])
+def check_task():
+    task_id = request.json.get('task_id')
+    task = client[DB_NAME].todos.find_one({
+        "_id": ObjectId(task_id)
     })
 
-    return "Task created successfully!"
+    # there is a chance that task has no "done"
+    # so if there is no key named "done", we just set "done" to False
+    if task.get('done') is None:
+        task['done'] = False
 
-#### By default, date is stored as string
-#### use datetime.datetime.strptime()
-
+    client[DB_NAME].todos.update({
+        "_id": ObjectId(task_id)
+    }, {
+        '$set': {
+            'done': not task['done']
+        }
+    })
+    # if we return a dictionary in Flask, flask will auto-convert to JSON
+    return {
+        "status": "OK"
+    }
+# By default, date is stored as string
+# use datetime.datetime.strptime()
 
 
 # "magic code" -- boilerplate
